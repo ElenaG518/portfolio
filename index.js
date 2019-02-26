a4e9df6e4eb9dfd3f4bb0b6ce7b3e9aff1b2572d
Elena Granados <patty518@yahoo.com>
2019-02-25 18:23:40 -0700

    fix github links

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ $(window).scroll(function() {
   if ( $(this).scrollTop() > 250 ) {
     mainNav.addClass('fix-nav');
     mainSlideshow.addClass('hidden');
-    $('#about').addClass('scrolled');
+    // $('#about').addClass('scrolled');
   }
   else {
     mainNav.removeClass('fix-nav');
     mainSlideshow.removeClass('hidden');
-    $('#about').removeClass('scrolled');
+    // $('#about').removeClass('scrolled');
   }
 });
 
@@ -20,7 +20,7 @@ $(function() {
 
   $('a[href="#about"]').click(function() {
       console.log("about has been clicked");
-      $('#about').addClass('scrolled');
+      // $('#about').addClass('scrolled');
       $('a[href="#about"]').addClass("active");
       $('a[href="#webDev"]').removeClass('active');
       $('a[href="#skills"]').removeClass('active');
@@ -28,7 +28,7 @@ $(function() {
   });
   $('a[href="#webDev"]').click(function() {
       console.log("webdev has been clicked");
-      $('#webDev').addClass('scrolled-less');
+      // $('#webDev').addClass('scrolled-less');
       $('a[href="#webDev"]').addClass("active");
       $('a[href="#about"]').removeClass('active');
       $('a[href="#skills"]').removeClass('active');
@@ -44,7 +44,7 @@ $(function() {
   });
   $('a[href="#contact"]').click(function() {
     console.log("contact has been clicked");
-    $('#contact').addClass('scrolled-less');
+    // $('#contact').addClass('scrolled-less');
     $('a[href="#contact"]').addClass("active");
     $('a[href="#webDev"]').removeClass('active');
     $('a[href="#skills"]').removeClass('active');
